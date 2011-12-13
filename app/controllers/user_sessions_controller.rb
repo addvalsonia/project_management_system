6e95373024efbebd150b4fab8a012ff6ca01057c
faizan mohammad <mohammad.faizan@addvalsolutions.com>
2011-12-13 23:36:53 +0530

    sign up

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,7 +6,7 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
       if @user_session.save
         flash[:notice] = "Successfully logged in."
-        redirect_to root_url
+        redirect_to new_user_path
       else
         render :action => 'new'
       end
--- config/routes.rb
+++ config/routes.rb
@@ -42,7 +42,7 @@ ActionController::Routing::Routes.draw do |map|
   # map.root :controller => "welcome"
 
   # See how all your routes lay out with "rake routes"
-
+  map.root :controller => "user_sessions", :action => "new" 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
